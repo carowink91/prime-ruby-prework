07d2b8e190d552cf5c3521ed18fe9a56a1d32d02
angelica winkler <caw400@nyu.edu>
2018-07-18 21:34:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime? (n)
+  array = []
+  range = (2..n-1)
+
+  range.each do |i|
+    array.push()
+  end
+
+
+  primeArray = []
+
+  array.each do |i|
+    primeArray.push(n % i)
+    end
+
+  if n <= 1
+    return false
+  elsif primeArray.include?(0) == true
+    return false
+  else
+    return true
+  end
+end
